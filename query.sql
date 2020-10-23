b6f8256177f22dd9eabd88a280ca7962d0b9f8f7
Howakyl96 <howakyl@gmail.com>
2020-10-23 15:56:38 -0700

    initial commit - clues #1 and #2 answered

---
+++ query.sql
@@ -0,0 +1,12 @@
+-- SELECT name, code, population 
+-- FROM country
+-- WHERE region = 'Southern Europe'
+-- ORDER BY population;
+
+-- Holy See (Vatican City State) VAT 1000
+
+-- SELECT language 
+-- FROM countrylanguage
+-- WHERE countrycode  = 'VAT';
+
+-- LANGUAGE IS: Italian
